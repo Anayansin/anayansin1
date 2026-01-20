d31a5e60a81601b1e2342fff973a8dd46bd7cdfb
Anayansin <yaretzicramos@gmail.com>
2026-01-19 19:14:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function alerta() {
   alert("Me quedo bien onichan");
 }
-const boton = document.getElementById(cambiarTitulo);
-const titulo = document.getElementById(titulo);
+const boton = document.getElementById("cambiarTitulo");
+const titulo = document.getElementById("titulo");
 
 boton.addEventListener("click", function () {
 titulo.textContent = "Anayansin :D";
